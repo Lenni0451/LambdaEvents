d12484f1b1114be6e4298f8c0874ff482a6906f9
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2021-09-27 20:25:19 +0200

    Testing

--- src/main/test/test/Test.java
+++ src/main/test/test/Test.java
@@ -7,16 +7,18 @@ public class Test {
 
     public static void main(String[] args) {
         LambdaManager.global().register(Test.class);
-        LambdaManager.global().call("Hi Kevin 1");
-        LambdaManager.global().unregister(Test.class);
-        LambdaManager.global().call("Hi Kevin 2");
-        LambdaManager.global().register(Test.class);
-        LambdaManager.global().call("Hi Kevin 3");
+        LambdaManager.global().call("1337");
+        LambdaManager.global().call(1337);
     }
 
     @LambdaHandler
     public static void test(final String s) {
-        System.out.println(s);
+        System.out.println("String: " + s);
+    }
+
+    @LambdaHandler
+    public static void test(final Integer i) {
+        System.out.println("Integer: " + i);
     }
 
 }
